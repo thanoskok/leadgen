006ddac2e11e1b2950d183d403403229ecde5be0
thanoskok <148118017+thanoskok@users.noreply.github.com>
2024-02-20 01:18:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('strategy-call-btn').addEventListener('click', function() {
+  document.querySelector('.calendar-section').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+});
